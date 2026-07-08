9ec77feff94d4e5e786cffce6593964282e3bed6
SidharthBsunil <sidharthbsunil@gmail.com>
2026-07-08 20:50:39 +0530

    add operation in one line

--- ASSIGNMENTNO_1/2.c
+++ ASSIGNMENTNO_1/2.c
@@ -12,15 +12,22 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    unsigned int registerpart1;
+    unsigned int registerpart1,duplicateregister;
     printf("enter the number \n");
-    scanf("%d",&registerpart1);
+    scanf("%u",&registerpart1);
+    duplicateregister=registerpart1; //for do operation in one line
+    // doing operation by step by step
     registerpart1 ^=1;
     printf("after toggling in first bit is %u\n",registerpart1);
     registerpart1|=(1<<2);
     printf("after setting third bit %u\n",registerpart1);
     registerpart1&=~(1<<5);
     printf("after clearing sixth bit %u\n",registerpart1);
+    //doing operation in one line
+    printf("  ");
+    duplicateregister=(((duplicateregister^(1<<0))|(1<<2))&~(1<<5));
+     printf("after doing operation in one line %u\n",duplicateregister);
+
 
 
 }
